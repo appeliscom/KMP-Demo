763e9035e2906a31925b30c10b22f14399b89cd2
honzamalousek <78351282+honzamalousek@users.noreply.github.com>
2024-04-03 10:09:27 +0200

    minor changes

--- features/leaflet/leaflet-data/src/androidMain/kotlin/com/appelis/kmp_demo/leaflet_data/repository/LeafletClient.android.kt
+++
@@ -1,7 +0,0 @@
-package com.appelis.kmp_demo.leaflet_data.repository
-
-import com.appelis.kmp_demo.leaflet_data.client.LeafletSuspendClient
-
-actual class LeafletSuspendClientImpl : LeafletSuspendClient {
-
-}
