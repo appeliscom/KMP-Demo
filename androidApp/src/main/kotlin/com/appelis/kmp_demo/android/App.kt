74294e7392c8244ee42b620c8e27cab4df073210
honzamalousek <78351282+honzamalousek@users.noreply.github.com>
2024-04-09 08:55:39 +0200

    DI initialization

---
+++ androidApp/src/main/kotlin/com/appelis/kmp_demo/android/App.kt
@@ -0,0 +1,23 @@
+package com.appelis.kmp_demo.android
+
+import android.app.Application
+import com.appelis.kmp_demo.KmpApplication
+import com.appelis.kmp_demo.android.di.nativeModule
+import com.appelis.kmp_demo.di.makePlatformGrpcClientModule
+import com.appelis.kmp_demo.leaflet_data.LeafletSuspendClientImpl
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initializeKmp()
+    }
+
+    private fun initializeKmp() {
+        KmpApplication.initSharedModule(
+            nativeModule = nativeModule,
+            platformGrpcClientModule = makePlatformGrpcClientModule(
+                LeafletSuspendClientImpl()
+            )
+        )
+    }
+}
